cc32c67147e3a4cf24f30cf167c9a21201c27b0f
SaumyaRai2010 <saunya.rai2010@gmail.com>
2022-02-10 20:08:15 +0530

    cleared comments

--- src/main/java/model/ParkingLot.java
+++ src/main/java/model/ParkingLot.java
@@ -10,19 +10,6 @@ public class ParkingLot {
     public ParkingLot() {
         this.parkingVehicleList = new ArrayList<>();
     }
-
-//    private static List<model.ParkingVehicle> parkingVehicleList;
-//        private static List<model.ParkingVehicle> parkingLot1 = new ArrayList<ParkingVariables>(Collections.nCopies(3, 0));
-//    private static HashMap<String, model.ParkingVehicle> parkingLot1 = new HashMap<>();
-//    public static HashMap<String, model.ParkingVehicle> getParkingLot1() {
-//        return parkingLot1;
-//    }
-//
-//    public static void setParkingLot1(HashMap<String, model.ParkingVehicle> parkingLot1) {
-//        model.ParkingLot.parkingLot1 = parkingLot1;
-//    }
-
-
     public List<ParkingVehicle> getParkingVehicleList() {
         return parkingVehicleList;
     }
